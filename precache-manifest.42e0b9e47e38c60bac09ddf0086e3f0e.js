208d61e62bd95c659aab607feb3222b29336769e
Alexandre Campos <camposalexandreh@gmail.com>
2019-05-06 14:47:19 -0300

    Updates

---
+++ precache-manifest.42e0b9e47e38c60bac09ddf0086e3f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286dccf805cd43afc67659654ea56978",
+    "url": "/react-pixel-tilling/index.html"
+  },
+  {
+    "revision": "e2a7c108e69a3fb46a88",
+    "url": "/react-pixel-tilling/static/css/main.cb73105f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-pixel-tilling/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e2a7c108e69a3fb46a88",
+    "url": "/react-pixel-tilling/static/js/main.80d7c286.chunk.js"
+  },
+  {
+    "revision": "e5837652a1e6d810520a",
+    "url": "/react-pixel-tilling/static/js/runtime~main.ba32b6b6.js"
+  }
+]);
